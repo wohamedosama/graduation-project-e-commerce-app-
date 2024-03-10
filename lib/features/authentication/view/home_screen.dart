b6485f547383fe50576d4efb9ef5804b3219a734
woahmedosmaa <mohamedmedo9932@gmail.com>
2024-03-10 07:17:18 +0200

    Last Update

--- lib/features/authentication/view/home_screen.dart
+++ lib/features/authentication/view/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/common/widget/custom_shapes/container/primary_header_container.dart';
 import 'package:e_commerce/common/widget/custom_shapes/container/search_container.dart';
-import 'package:e_commerce/common/widget/image_text/vertical_image_text.dart';
+import 'package:e_commerce/common/widget/layout/grid_layout.dart';
+import 'package:e_commerce/common/widget/products/products_card/products_cards_vertical.dart';
 import 'package:e_commerce/common/widget/texts/section_heading.dart';
 import 'package:e_commerce/features/shop/view/home/widget/home_appbar.dart';
+import 'package:e_commerce/features/shop/view/home/widget/home_categories.dart';
+import 'package:e_commerce/features/shop/view/home/widget/promo_slider.dart';
 import 'package:e_commerce/utils/constants/images_string.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -16,49 +19,64 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            PrimaryHeaderContainer(
+            //Header
+            const PrimaryHeaderContainer(
               child: Column(
                 children: [
                   // App bar With cart icon , title and subtitle
-                  const HomeAppbar(),
-                  const SizedBox(height: MySize.spaceBtwSections),
+                  HomeAppbar(),
+                  SizedBox(height: MySize.spaceBtwSections),
 
                   //searchbar
-                  const SearchContainer(text: 'Search In The Store'),
-                  const SizedBox(height: MySize.spaceBtwSections),
+                  SearchContainer(text: 'Search In The Store'),
+                  SizedBox(height: MySize.spaceBtwSections),
 
                   //  -- categories
 
                   Padding(
-                    padding: const EdgeInsets.only(left: MySize.defaultSpace),
+                    padding: EdgeInsets.only(left: MySize.defaultSpace),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         // Heading
-                        const SectionHeading(
+                        SectionHeading(
                           title: 'Popular Categories',
                           showActionButton: false,
                         ),
-                        const SizedBox(height: MySize.spaceBtwItems),
+                        SizedBox(height: MySize.spaceBtwItems),
 
                         //Categories
-                        SizedBox(
-                          height: 80,
-                          child: ListView.builder(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 10,
-                              itemBuilder: (_, index) {
-                                return VerticalImageText(
-                                  onTap: () {},
-                                  title: 'shoes',
-                                  image: MyImages.sportIcon,
-                                );
-                              }),
-                        )
+                        HomeCategories()
                       ],
                     ),
                   ),
+                ],
+              ),
+            ),
+
+            //Body
+            Padding(
+              padding: const EdgeInsets.all(MySize.defaultSpace),
+              child: Column(
+                children: [
+                  //Promo Slider
+                  const PromoSlider(
+                    banners: [
+                      MyImages.banner4,
+                      MyImages.banner7,
+                      MyImages.banner1
+                    ],
+                  ),
+                  const SizedBox(height: MySize.spaceBtwItems),
+
+                  //Heading
+                  SectionHeading(title: 'Popular Products', onPressed: () {}),
+                  const SizedBox(height: MySize.spaceBtwItems),
+
+                  //Popular Products
+                  GridLayout(
+                      itemCount: 10,
+                      itemBuilder: (_, index) => const ProductCardVertical())
                 ],
               ),
             ),
@@ -68,3 +86,8 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+// child: const PromoSlider(banners: [
+// MyImages.banner4,
+// MyImages.banner7,
+// MyImages.banner1
+// ]),
