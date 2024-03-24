93aadcb06a86f27dbb41cc2625fec7fae752ccea
woahmedosmaa <mohamedmedo9932@gmail.com>
2024-03-25 00:59:28 +0200

    End of Profile screen

--- lib/common/widget/custom_shapes/container/search_container.dart
+++ lib/common/widget/custom_shapes/container/search_container.dart
@@ -12,35 +12,42 @@ class SearchContainer extends StatelessWidget {
     this.icon = Iconsax.search_normal,
     this.showBackGround = true,
     this.showBorder = true,
+    this.onTap,
+    this.padding = const EdgeInsets.symmetric(horizontal: MySize.defaultSpace),
   });
 
   final String text;
   final IconData? icon;
   final bool showBackGround, showBorder;
+  final VoidCallback? onTap;
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
     final dark = HelperFunction.isDark(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: MySize.defaultSpace),
-      child: Container(
-        width: HelperFunction.screenWidth(context),
-        padding: const EdgeInsets.all(MySize.md),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(MySize.cardRadiusLg),
-          color: showBackGround
-              ? dark
-                  ? MyColors.dark
-                  : MyColors.white
-              : Colors.transparent,
-          border: showBorder ? Border.all(color: MyColors.grey) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: MyColors.darkGrey),
-            const SizedBox(width: MySize.spaceBtwItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall)
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: padding,
+        child: Container(
+          width: HelperFunction.screenWidth(context),
+          padding: const EdgeInsets.all(MySize.md),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(MySize.cardRadiusLg),
+            color: showBackGround
+                ? dark
+                    ? MyColors.dark
+                    : MyColors.white
+                : Colors.transparent,
+            border: showBorder ? Border.all(color: MyColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: MyColors.darkGrey),
+              const SizedBox(width: MySize.spaceBtwItems),
+              Text(text, style: Theme.of(context).textTheme.bodySmall)
+            ],
+          ),
         ),
       ),
     );
